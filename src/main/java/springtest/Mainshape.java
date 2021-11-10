66d07839413d9d16f8be850b8f2d8d1223ebe337
bharadwajallu <33192990+bharadwajallu@users.noreply.github.com>
2021-11-10 22:29:24 +0530

    Add files via upload

---
+++ src/main/java/springtest/Mainshape.java
@@ -0,0 +1,18 @@
+package springtest;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Mainshape{
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("shape.xml");
+		context.registerShutdownHook();  
+		Shape obj=(Shape) context.getBean("shape");
+		obj.draw();
+		  
+
+	}
+ 
+}
